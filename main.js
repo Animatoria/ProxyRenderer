b1bb3770e38774a8cc97067bc11dfe30be07d17c
Animatoria <nn.animatoria@gmail.com>
2019-02-20 14:32:12 +0300

    request

---
+++ main.js
@@ -0,0 +1,16 @@
+var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
+
+var xhr = new XHR();
+
+// (2) запрос на другой домен :)
+xhr.open('GET', 'https://proxyfordevelopers.com/api/proxies/?format=json', true);
+
+xhr.onload = function() {
+  alert( this.responseText );
+}
+
+xhr.onerror = function() {
+  alert( 'Ошибка ' + this.status );
+}
+
+xhr.send();
