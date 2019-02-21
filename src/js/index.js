dccae3036cf80bdca9a0b29af1a76e244f6057a0
Animatoria <nn.animatoria@gmail.com>
2019-02-21 19:25:32 +0300

    beta

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,52 @@
 import './../scss/index.scss';
 
+var vm = new Vue({
+  el: '#landing',
+  data: {
+    proxyList: [],
+    proxyType: ['Transparent', 'Anonymous', 'Elite'],
+    proxyDetails: {},
+    country: null
+  },
+  computed: {
+    aliveProxy: function() {
+
+      var list = [];
+
+      this.proxyList.forEach(function(proxy){
+        if (proxy.alive === true && proxy.country === vm.country) list.push(proxy);
+      })
+
+      return list
+    },
+    countries: function() {
+
+      var list = new Set;
+
+      this.proxyList.forEach(function(proxy){
+        list.add(proxy.country);
+      })
+
+      return list
+    }
+  },
+  methods: {
+    showDetails: function(id) {
+      this.aliveProxy.forEach(function(proxy) {
+        if (proxy.id === id) vm.proxyDetails = proxy
+      })
+    }
+  }
+});
+
 var XHR = ("onload" in new XMLHttpRequest()) ? XMLHttpRequest : XDomainRequest;
 
 var xhr = new XHR();
 
-xhr.open('GET', 'https://proxyfordevelopers.com/api/proxies/?format=json', true);
+xhr.open('GET', 'proxy.json', true);
 
 xhr.onload = function() {
-  alert( this.responseText );
+  vm.proxyList = JSON.parse(this.responseText);
 }
 
 xhr.onerror = function() {
